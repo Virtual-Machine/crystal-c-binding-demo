ff91ed3178ac96f2f90ddf7d593b90ac43159a3c
Virtual-Machine <darrelm@tech-asteroid.com>
2016-11-23 05:12:08 -0500

    Add really basic c library code

---
+++ example_lib.c
@@ -0,0 +1,9 @@
+#include "example.h"
+
+char * helloWorld() {
+	return "Hello there\0";
+}
+
+int addSomeNumbers(int a, int b) {
+	return a + b;
+}
---
+++ example_lib.h
@@ -0,0 +1,7 @@
+#ifndef _EXAMPLELIB_H_
+#define _EXAMPLELIB_H_
+
+    extern char * helloWorld();
+    extern int addSomeNumbers(int, int);
+
+#endif
